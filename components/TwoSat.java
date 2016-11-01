58b11818dee15877257d7f1c2f27fe3ff14fff3f
Stanley Nguyen <hung.ngn.the@gmail.com>
2016-11-01 13:54:23 +0800

    graph construction

--- components/TwoSat.java
+++ components/TwoSat.java
@@ -4,6 +4,7 @@ import java.util.*;
 import java.io.*;
 
 public class TwoSat {
+    private Graph graph = new Graph();
 
     TwoSat() {};
 
@@ -14,6 +15,26 @@ public class TwoSat {
         for ( int[] c : p.getClauses() ) {
             if ( c.length != 2 ) throw new InvalidInputException();
         }
+        //build Graph
+        int flippedFirst, flippedSecond;
+        for ( int[] c : p.getClauses() ) {
+            //instantiate new nodes and add to graph if not in yet
+            Node nodeFromFirst = new Node(c[0]);
+            if ( graph.indexOf(nodeFromFirst) == -1 ) graph.add(nodeFromFirst.clone());
+            Node nodeFromSecond = new Node(c[1]);
+            if ( graph.indexOf(nodeFromSecond) == -1 ) graph.add(nodeFromSecond.clone());
+            flippedFirst = 0 - c[0];
+            Node nodeFromFlippedFirst = new Node(flippedFirst);
+            if ( graph.indexOf(nodeFromFlippedFirst) == -1 ) graph.add(nodeFromFlippedFirst.clone());
+            flippedSecond = 0 - c[1];
+            Node nodeFromFlippedSecond = new Node(flippedSecond);
+            if ( graph.indexOf(nodeFromFlippedSecond) == -1 ) graph.add(nodeFromFlippedSecond.clone());
+            //add children to nodes
+            graph.getNodeFromVal(flippedFirst).addAChild(graph.getNodeFromVal(c[1]));
+            graph.getNodeFromVal(flippedSecond).addAChild(graph.getNodeFromVal(c[0]));
+        }
+
+        System.out.println(graph);
     }
 
     public String solve() {
