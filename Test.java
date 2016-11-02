984b04c6bf1b9f1ecc3883ee9643450bfbc8912c
Stanley Nguyen <hung.ngn.the@gmail.com>
2016-11-02 14:04:42 +0800

    working version

--- Test.java
+++ Test.java
@@ -5,15 +5,21 @@ class Test {
 
     public static void testCase(String filePath) {
         Parser p = new Parser(filePath);
-        System.out.println(p);
+        // System.out.println(p);
+        TwoSat t = new TwoSat();
+        final long startTime = System.currentTimeMillis();
         try {
-            TwoSat t = new TwoSat(p);
+            t = new TwoSat(p);
         } catch (InvalidInputException e) {
             System.out.println("Program ended due to invalid input file!");
+        } finally {
+            System.out.println(t.solve());
+            final long endTime = System.currentTimeMillis();
+            System.out.println("Total execution time: " + (endTime - startTime) );
         }
     }
 
     public static void main(String[] args) {
-        testCase("file.txt");
+        testCase("largeSat.cnf");
     }
 }
--- components/TwoSat.java
+++ components/TwoSat.java
@@ -7,9 +7,10 @@ public class TwoSat {
     private Graph graph = new Graph();
     private Graph transposedGraph = new Graph();
     private ArrayList<Integer> stack = new ArrayList<Integer>();
-    private int[][] scc;
+    private ArrayList<ArrayList<Integer>> scc = new ArrayList<ArrayList<Integer>>();
+    private HashMap<Integer, Integer> output = new HashMap<Integer, Integer>();
 
-    TwoSat() {};
+    public TwoSat() {};
 
     public TwoSat(Parser p) throws InvalidInputException {
         //check for valid input file type
@@ -20,22 +21,21 @@ public class TwoSat {
         }
         //build Graph
         graph.add(new Node(0)); //virtual almighty root node
-        transposedGraph.add(new Node(0)); //virtual almighty root node
+        // transposedGraph.add(new Node(0)); //virtual almighty root node
         int flippedFirst, flippedSecond;
         for ( int[] c : p.getClauses() ) {
-            System.out.println(Arrays.toString(c));
             //instantiate new nodes and add to graph if not in yet
             Node nodeFromFirst = new Node(c[0]);
             if ( graph.indexOf(nodeFromFirst) == -1 ) {
                 graph.add(nodeFromFirst.clone());
                 transposedGraph.add(nodeFromFirst.clone());
-                transposedGraph.getNodeFromVal(0).addAChild(transposedGraph.getNodeFromVal(c[0])); //connect yo virtual root
+                // transposedGraph.getNodeFromVal(0).addAChild(transposedGraph.getNodeFromVal(c[0])); //connect yo virtual root
             }
             Node nodeFromSecond = new Node(c[1]);
             if ( graph.indexOf(nodeFromSecond) == -1 ) {
                 graph.add(nodeFromSecond.clone());
                 transposedGraph.add(nodeFromSecond.clone());
-                transposedGraph.getNodeFromVal(0).addAChild(transposedGraph.getNodeFromVal(c[1])); //connect yo virtual root
+                // transposedGraph.getNodeFromVal(0).addAChild(transposedGraph.getNodeFromVal(c[1])); //connect yo virtual root
             }
             flippedFirst = 0 - c[0];
             Node nodeFromFlippedFirst = new Node(flippedFirst);
@@ -65,36 +65,68 @@ public class TwoSat {
                 transposedGraph.getNodeFromVal(c[0]).addAChild(transposedGraph.getNodeFromVal(flippedSecond));
             }
         }
-
-        System.out.println(graph);
-        System.out.println(transposedGraph);
-        dfs(graph.getNodeFromVal(0));
-        System.out.println(stack);
+        //instantiate output
+        // this.output = new int[p.getVarNo()];
     }
 
     public void dfs(Node n) {
         if ( n.getState() == 1 ) {
-            System.out.println("adding " + n);
             n.incState();
             if ( n.getValue() != 0 ) stack.add(n.getValue());
         } else if ( n.getState() == 0 ) {
             n.incState();
             ArrayList<Node> untravelledChildren = n.getUntravelledChildren();
-            if ( untravelledChildren == null ) {
-                dfs(n);
-            } else {
+            if ( untravelledChildren != null ) {
                 for ( Node c : untravelledChildren ) {
-                    System.out.println("travelling " + c);
                     dfs(c);
                 }
-                dfs(n);
             }
-        } else {
-            System.out.println("state 2 ah?");
+            dfs(n);
+        }
+    }
+
+    public void buildSCC() {
+        int currentVal = stack.remove(stack.size() - 1), counter = 0;
+        Node currentNode;
+        ArrayList<Integer> currentTrace = new ArrayList<Integer>();
+        while ( stack.size() > 0 ) {
+            for ( int i = 1; i <= counter; i++ ) {
+                currentVal = stack.remove(stack.size() - 1);
+            }
+            currentNode = transposedGraph.getNodeFromVal(currentVal);
+            currentNode.incState();
+            currentTrace.add(currentNode.getValue());
+            counter = 0;
+            while ( currentNode.getUntravelledChildren() != null ) {
+                currentNode.incState();
+                currentNode = currentNode.getUntravelledChildren().get(0);
+                currentTrace.add(currentNode.getValue());
+                counter++;
+            }
+            if ( counter == 0 ) {
+                counter++;
+            }
+            scc.add(new ArrayList<Integer>(currentTrace));
+            currentTrace.clear();
         }
     }
 
     public String solve() {
+        this.dfs(graph.getNodeFromVal(0));
+        this.buildSCC();
+        for ( int j = this.scc.size() - 1; j > -1; j-- ) {
+            ArrayList<Integer> s = this.scc.get(j);
+            for ( int i : s ) {
+                if ( s.indexOf( 0 - i ) != -1 ) return "FORMULA UNSATISFIABLE";
+                else if ( i > 0 && !output.containsKey(i) ) output.put(i, 1);
+                else if ( i < 0 && !output.containsKey(0-i) ) output.put(0-i, 0);
+            }
+        }
+        String result = "";
+        for ( int i : output.keySet() ) {
+            result += output.get(i) + " ";
+        }
+        System.out.println("output: " + result);
         return "FORMULA SATISFIABLE";
     }
 }
